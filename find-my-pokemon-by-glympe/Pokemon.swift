3b1c034b8e142bc1aa30bf8db0fb8ffc61ebef11
Γιώργος Λυμπερόπουλος <glympe@me.com>
2016-01-17 20:57:44 +0200

    Completed the bio section of details view controller. Added a loading screen while downloading info from API. Next is the move section

--- find-my-pokemon-by-glympe/Pokemon.swift
+++ find-my-pokemon-by-glympe/Pokemon.swift
@@ -20,42 +20,80 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvoTxt: String!
+    private var _nextEvoId: String!
+    private var _nextEvoLvl: String!
     private var _pokemonUrl: String!
     
     var name: String {
-        return _name
+                return _name
     }
     
     var pokedexId: Int {
+        
         return _pokedexId
     }
     
-    var pokemonUrl: String {
-        return _pokemonUrl
-    }
-    
     var descPokemon: String {
+        if _description == nil {
+            _description = ""
+        }
         return _description
     }
     
     var type: String {
+        if _type == nil {
+            _type = ""
+        }
         return _type
     }
     
     var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
         return _defense
     }
     
     var height: String {
+        if _height == nil {
+            _height = ""
+        }
         return _height
     }
     
     var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
         return _weight
     }
     
     var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
         return _attack
+    }
+    
+    var nextEvoTxt: String {
+        if _nextEvoTxt == nil {
+            _nextEvoTxt = ""
+        }
+        return _nextEvoTxt
+    }
+    
+    var nextEvoId: String {
+        if _nextEvoId == nil {
+            _nextEvoId = ""
+        }
+        return _nextEvoId
+    }
+    
+    var nextEvoLvl: String {
+        if _nextEvoLvl == nil {
+            _nextEvoLvl = ""
+        }
+        return _nextEvoLvl
     }
     
     init(name: String, pokedexId: Int){
@@ -75,9 +113,10 @@ class Pokemon {
     func downloadPokemonDetails(completed: DownloadComplete){
         //Asynchronus download (data will not be instantly available to detail controller)
         //We need to tell to viewController when download is complete
-        let url = NSURL(string: pokemonUrl)!
+        let url = NSURL(string: _pokemonUrl)!
         //Sending a request to url and getting the result when finished
         var descrUrl: String!
+        //var nextEvoUrl: String!
         
         Alamofire.request(.GET, url).responseJSON { response in
             let result = response.result
@@ -129,27 +168,64 @@ class Pokemon {
                     self._type = "No Types"
                 }
                 
+                //Checking and grabing next evolution lvl and link to pokemon
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0 {
+                    if let level = evolutions[0]["level"] as? Int {
+                        self._nextEvoLvl = "\(level)"
+                    }
+                    if let to = evolutions[0]["to"] as? String {
+                        //Can't support mega pokemon atm
+                        if to.rangeOfString("mega") == nil {
+                            if let str = evolutions[0]["resource_uri"] as? String {
+                                let newStr = str.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                                let num = newStr.stringByReplacingOccurrencesOfString("/", withString: "")
+                                self._nextEvoId = num
+                                self._nextEvoTxt = to
+//                                let endRange = str.endIndex.advancedBy(-1)..<str.endIndex
+//                                str.removeRange(endRange)
+//                                self._nextEvoTxt = str.substringFromIndex(str.startIndex.advancedBy(16))
+                            }
+                        }
+                    }
+//                    if let evoUrl = evolutions["resource_uri"] {
+//                        nextEvoUrl = "\(URL_BASE)\(evoUrl)"
+//                        //let evoUrl = NSURL(string: nextEvoUrl)!
+//                        //            Alamofire.request(.GET, evoUrl).responseJSON { response in
+//                        //                let result = response.result
+//                        //                if let dict = result.value as? Dictionary<String, AnyObject>{
+//                        //                    
+//                        //                }
+//                        //               completed()
+//                        //            }
+//
+//                    }else{
+//                        nextEvoUrl = ""
+//                    }
+                }
+                
+                //Checking and grabing the first available description
                 if let descriptions = dict["descriptions"] as? [Dictionary<String, String>] where descriptions.count > 0 {
                     if let descriptionUrl = descriptions[0]["resource_uri"]{
                         descrUrl  = "\(URL_BASE)\(descriptionUrl)"
+                        let newUrl = NSURL(string: descrUrl)!
+                        Alamofire.request(.GET, newUrl).responseJSON { response in
+                            
+                            let result = response.result
+                            if let dict = result.value as? Dictionary<String, AnyObject> {
+                                if let description = dict["description"] as? String{
+                                    self._description = description
+                                    //print(self._description)
+                                }
+                            }
+                            completed()
+                        }
+
                     }else{
-                        descrUrl = ""
+                        self._description = "No Descr"
                     }
                 }
             }
-            let newUrl = NSURL(string: descrUrl)!
-            Alamofire.request(.GET, newUrl).responseJSON { response in
-                let result = response.result
-                if let dict = result.value as? Dictionary<String, AnyObject> {
-                    if let description = dict["description"] as? String{
-                        self._description = description
-                    }
-                }else{
-                    self._description = "No Descr"
-                }
-                print(self._description)
-                
-            }
+            
         }
     }
 }
--- find-my-pokemon-by-glympe/PokemonDetailVC.swift
+++ find-my-pokemon-by-glympe/PokemonDetailVC.swift
@@ -13,39 +13,98 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var mainImg: UIImageView!
     
+    @IBOutlet weak var typeLbl: UILabel!
     @IBOutlet weak var descriptionLbl: UILabel!
-    @IBOutlet weak var typeLbl: UIStackView!
-    @IBOutlet weak var defenseLbl: UIStackView!
+    @IBOutlet weak var defLbl: UILabel!
     @IBOutlet weak var heightLbl: UILabel!
     @IBOutlet weak var pokedexLbl: UILabel!
     @IBOutlet weak var weightLbl: UILabel!
     @IBOutlet weak var attackLbl: UILabel!
     @IBOutlet weak var evoLbl: UILabel!
-    @IBOutlet weak var currentEvoLbl: UIImageView!
-    @IBOutlet weak var nextEvoLbl: UIImageView!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var activityView: UIView!
+    @IBOutlet weak var bgView: UIView!
     
-    
+        
     
     var pokemon: Pokemon!
     override func viewDidLoad() {
         super.viewDidLoad()
+        showActivityIndicatory(activityView)
         nameLbl.text = pokemon.name
         mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        segmentedControl.selectedSegmentIndex = 0
         pokemon.downloadPokemonDetails { () -> () in
             //This will be called after download is done
-            
-           
+            self.updateUI()
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.hidden = true
+            self.activityView.hidden = true
+            self.bgView.hidden = true
         }
-        
+        //activityIndicatorView.stopAnimating()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateUI(){
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.descPokemon
+        defLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        weightLbl.text = pokemon.weight
+        attackLbl.text = pokemon.attack
+        if pokemon.nextEvoId == "" {
+            evoLbl.text = "No evolution"
+            nextEvoImg.hidden = true
+        }else{
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvoId)
+            var str = "Next Evolution: \(pokemon.nextEvoTxt)"
+            if pokemon.nextEvoLvl != "" {
+                str += " - LVL: \(pokemon.nextEvoLvl)"
+            }
+            evoLbl.text = str
+        }
+        currentEvoImg.image = mainImg.image
+    }
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            typeLbl.text = pokemon.type
+        case 1:
+            typeLbl.text = "No Type"
+        default:
+            break; 
+        }
+    }
 
     @IBAction func backBtn(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+        
     }
-}
+    
+    func showActivityIndicatory(uiView: UIView) {
+        //let actInd: UIActivityIndicatorView = UIActivityIndicatorView()
+        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        activityIndicator.center = uiView.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle =
+        UIActivityIndicatorViewStyle.WhiteLarge
+        uiView.addSubview(activityIndicator)
+        bgView.layer.cornerRadius = 10.0
+        bgView.clipsToBounds = true
+        activityIndicator.startAnimating()
+        
+    }
+    
+    }
